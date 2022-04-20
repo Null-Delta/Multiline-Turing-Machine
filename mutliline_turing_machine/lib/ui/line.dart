520752ca7cc84ebe4ff30ea9c80b819547b583f3
iamgirya <iamgirya@yandex.ru>
2022-04-21 00:51:22 +0300

    Линия с цифрами

--- mutliline_turing_machine/lib/ui/line.dart
+++ mutliline_turing_machine/lib/ui/line.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import '../scrollAbleList/scrollable_positioned_list.dart';
 import 'line_cell.dart';
-import 'scrollable_positioned_list.dart';
 
 class Line extends StatefulWidget {
   const Line({Key? key}) : super(key: key);
@@ -11,29 +11,47 @@ class Line extends StatefulWidget {
 
 class _LineState extends State<Line> {
   
+  static const double _widthOfCell = 28;
+  static const double _widthOfSeparator = 4;
+
+  int cellCount = 2001;
   ItemScrollController control = ItemScrollController();
 
   late var line =  ScrollablePositionedList.separated(
             itemScrollController: control,
             scrollDirection: Axis.horizontal,
-            itemCount: 101,
+            itemCount: cellCount,
             initialScrollIndex: 0,
             itemBuilder: (context, index) { 
               return Stack(
                 children: [
                   const LineCell(),
-                  Text((index-50).toString())
+                  Container (
+                    padding: const EdgeInsets.fromLTRB(0, 56, 0, 0),
+                    child: SizedBox(
+                    width: 28,
+                    height: 20,
+                    child:  Text(
+                      (index-cellCount~/2).toString(),
+                       textAlign: TextAlign.center,
+                       style: const TextStyle(
+                          fontSize: 10, 
+                        )
+                      )
+                    )
+                  )
+                  
                 ],
               );
              },
-            separatorBuilder: (context, index) { return const SizedBox( width: 4); }
+            separatorBuilder: (context, index) { return const SizedBox( width: _widthOfSeparator); }
             );    
  
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
-      control.jumpTo(index: 101~/2, alignment: 0.5, myIndent: 14);
+      control.jumpTo(index: cellCount~/2, alignment: 0.5, myIndent: _widthOfCell/2);
       });
   }
 
@@ -41,18 +59,16 @@ class _LineState extends State<Line> {
   Widget build(BuildContext build) {
 
     if (control.isAttached){
-        control.jumpTo(index: 101~/2, alignment: 0.5, myIndent: 14);
+        control.jumpTo(index: cellCount~/2, alignment: 0.5, myIndent: _widthOfCell/2);
     }
 
     return GestureDetector(
       onTap: () {  
       },
-      onSecondaryTap: () {
-      },
       child: Align(alignment: Alignment.center,
       child: SizedBox(
           width: MediaQuery.of(context).size.width,
-          height: 50,
+          height: 67,
           child: line
         ),
       ) 
